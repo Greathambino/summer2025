46d39e1b2a4fa1986db3df450f5d47f59ce172d9
Wyatt Hamabe <wyattearp2004@gmail.com>
2025-07-22 13:49:26 -0700

    rest of the code

--- weather.py
+++ weather.py
@@ -14,7 +14,26 @@ pypi.org <-- search for packages online (documentation)
 import requests
 from dotenv import load_dotenv
 import os
+from pprint import pprint
 
 load_dotenv()  # Load environment variables from .env file
 
-requestURL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=imperial'
+def get_current_weather():
+    print(f'\n*** Get Current Weather Conditions ***\n')
+
+    city = input(f'\nPlease enter a city name:\n')
+
+    requestURL = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    #print(requestURL) # Uncomment to see the request URL
+
+    weather_data = requests.get(requestURL).json()
+    #pprint(weather_data) # Uncomment to see the full weather data
+
+    print(f'\nCurrent weather for {weather_data["name"]}')
+    print(f'\nThe temp is {weather_data["main"]["temp"]} degrees Fahrenheit')
+    print(f'\n{weather_data["weather"][0]["description"].capitalize()} and feels like {weather_data["main"]["feels_like"]} degrees Fahrenheit.')
+    
+
+if __name__ == "__main__":
+    get_current_weather()
+
