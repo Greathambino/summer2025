17255711dfbee192af938f5848ffaa29a9f92af9
Wyatt Hamabe <wyattearp2004@gmail.com>
2025-08-06 10:22:14 -0700

    basic html + flask route to index.html

--- hello.py
+++ hello.py
@@ -5,6 +5,6 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello_world(name=None):
+    return render_template('index.html', person=name)
 
